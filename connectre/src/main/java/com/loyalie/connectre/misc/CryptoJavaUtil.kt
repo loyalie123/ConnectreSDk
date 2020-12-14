ff68f0de0df7f3ca94fe3ebff90cc31e91dba01e
ribinafocaloid <ribina.salam@focaloid.com>
2020-12-14 10:45:22 +0530

    initial commit

--- connectre/src/main/java/com/loyalie/connectre/misc/CryptoJavaUtil.kt
+++ connectre/src/main/java/com/loyalie/connectre/misc/CryptoJavaUtil.kt
@@ -50,29 +50,21 @@ object CryptoJavaUtil {
         }
     }
   fun DecryptL(stringToEncode: String?, Key: String?): String? {
-        return try { /*from www  .  ja v  a 2s. c  o  m*/
+        return try {
             if (Key == null) {
                 return null
             }
-//            val vj=encrypt("{\"status\":1,\"message\":\"Success\",\"tokenStatus\":1,\"result\":{\"type1\":\"AIzaSyB_UgCRs99Z4h4YJ9Ihv5G73Xzv45BdJNw\",\"type2\":\"8be6ad7e541c15dc4a5767edd8e9e4b1\",\"type3\":\"AIzaSyDqhI-TfpaLuX0L5OaoBitUIQzwkiUYFds\",\"type4\":\"AIzaSyAtMbuX0dz0-8qH5Pv8Biat_3E36uPNg7s\",\"type5\":\"72e?5ve#7fa4*exi\"}}",WingmanApp.instance.getCin())
-//         Log.d("OkHtppen", vj)
+
 
             val raw = Key.toByteArray(charset("UTF-8"))
             val skeySpec = SecretKeySpec(raw, "AES")
             val cipher: Cipher
 
-//            byte[] encrypted1 = Base64.decode(stringToEncode, Base64.URL_SAFE);
-//            val encrypted1 = Base64.decode(vj, Base64.NO_WRAP)
             val encrypted1 = Base64.decode(stringToEncode, Base64.URL_SAFE)
-            //            if (null != IV) {
-//                IvParameterSpec ivParameterSpec = new IvParameterSpec(IV.getBytes());
-//                SecretKeySpec skeySpec = getKey(Key);
-//                cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-//                cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivParameterSpec);
-//            } else {
+
             cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
             cipher.init(Cipher.DECRYPT_MODE, skeySpec)
-            //            }
+
             val ret = cipher.doFinal(encrypted1)
             String(ret, Charset.forName("UTF-8"))
         } catch (ex: Exception) {
@@ -93,24 +85,13 @@ object CryptoJavaUtil {
     }
 
     fun encrypt(strToEncrypt: String, encryptKey: String): String {
-    /*    return try {
-            val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
-            val secretKey = SecretKeySpec(encryptKey.toByteArray(charset("UTF-8")), "AES")
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey)
 
-            *//*Base64Utils.encodeUrlSafe(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));*//*
-            Base64.encodeToString(cipher.doFinal(strToEncrypt.toByteArray(charset("UTF-8"))), Base64.DEFAULT)
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            "Cannot Encrypt Data"
-        }*/
      return try {
         val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
         val secretKey = SecretKeySpec(encryptKey.toByteArray(charset("UTF-8")), "AES")
         cipher.init(Cipher.ENCRYPT_MODE, secretKey)
 
-        /*Base64Utils.encodeUrlSafe(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));*/
+
         Base64.encodeToString(cipher.doFinal(strToEncrypt.toByteArray(charset("UTF-8"))), Base64.DEFAULT)
 
     } catch (e: Exception) {
